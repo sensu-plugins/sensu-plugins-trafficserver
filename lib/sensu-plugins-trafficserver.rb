eab978733a582bba05cb3024c78632b61e78dadc
Matt Jones <mattjones@yieldbot.com>
2015-06-01 21:42:11 -0400

    update repo

--- lib/sensu-plugins-trafficserver.rb
+++ lib/sensu-plugins-trafficserver.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-trafficserver/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsTrafficserver
-  class << self
-  end
-
-  class << self
-  end
-end
--- lib/sensu-plugins-trafficserver/version.rb
+++ lib/sensu-plugins-trafficserver/version.rb
@@ -1,7 +1,4 @@
-require 'json'
-
-# encoding: utf-8
-module SensuPluginsTrafficserver
+module SensuPluginsTrafficServer
   # This defines the version of the gem
   module Version
     MAJOR = 0
@@ -9,20 +6,5 @@ module SensuPluginsTrafficserver
     PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-
-    NAME   = 'sensu-plugins-trafficserver'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, VER_STRING)
-    end
-
-    def json_version
-      {
-        'version' => VER_STRING
-      }.to_json
-    end
   end
 end
